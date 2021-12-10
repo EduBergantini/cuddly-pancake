80babc18e3fd9c06d6bd0f66d16b1a44e142cf2e
Eduardo Bergantini <eduardo.bergantini@catenait.com.br>
2021-12-10 19:13:11 -0300

    feat: add authentication use case

---
+++ src/domain/use-cases/authentication.ts
@@ -0,0 +1,10 @@
+import { AccountModel } from "../models/account-models";
+
+type AuthenticationModel = {
+  email: string,
+  password: string
+}
+
+export interface IAuthentication {
+  auth (model: AuthenticationModel): Promise<AccountModel>;
+}
