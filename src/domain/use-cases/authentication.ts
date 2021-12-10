cb0ffa18b715fd258289383c76127cffc8c0add4
Eduardo Bergantini <eduardo.bergantini@catenait.com.br>
2021-12-10 19:48:20 -0300

    feat: ensure RemoteAuthentication calls IHttpPostClient with correct url

--- src/domain/use-cases/authentication.ts
+++ src/domain/use-cases/authentication.ts
@@ -1,10 +1,10 @@
-import { AccountModel } from "../models/account-models";
+import { AccountModel } from '../models/account-models'
 
 type AuthenticationModel = {
-  email: string,
+  email: string
   password: string
 }
 
 export interface IAuthentication {
-  auth (model: AuthenticationModel): Promise<AccountModel>;
+  auth: (model: AuthenticationModel) => Promise<AccountModel>
 }
