2effdd477b4daef50c7da0f86c178e3ede834cf4
Eduardo Bergantini <eduardo.bergantini@catenait.com.br>
2021-12-11 18:08:30 -0300

    refactor: move HttpPostParams creation to a factory

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -1,6 +1,7 @@
 import { AxiosHttpClient } from './axios-http-client'
 import axios from 'axios'
 import faker from 'faker'
+import { HttpPostParams } from '@/data/protocols/http'
 
 jest.mock('axios')
 const mockedAxios = axios as jest.Mocked<typeof axios>
@@ -9,14 +10,16 @@ const makeSut = (): AxiosHttpClient => {
   return new AxiosHttpClient()
 }
 
+const mockHttpPostParams = (): HttpPostParams<any> => ({
+  url: faker.internet.url(),
+  body: faker.random.objectElement()
+})
+
 describe('Axios Http Client', () => {
   test('Should call axios with correct url and http verb', async () => {
     const sut = makeSut()
-    const url = faker.internet.url()
-    await sut.post({
-      url,
-      body: undefined
-    })
-    expect(mockedAxios.post).toHaveBeenCalledWith(url)
+    const request = mockHttpPostParams()
+    await sut.post(request)
+    expect(mockedAxios.post).toHaveBeenCalledWith(request.url)
   })
 })
