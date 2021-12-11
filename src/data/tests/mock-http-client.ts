47dc136030aed61d7bdb0bc7505e1710606e0d4a
Eduardo Bergantini <eduardo.bergantini@catenait.com.br>
2021-12-11 01:31:25 -0300

    chore: move data tests folder inside data folder

--- src/data/tests/mock-http-client.ts
+++ src/data/tests/mock-http-client.ts
@@ -1,10 +1,12 @@
-import { HttpPostParams, IHttpPostClient } from '../data/protocols/http/http-post-client'
+import { HttpPostParams, IHttpPostClient } from '../protocols/http/http-post-client'
 
 export class HttpPostClientSpy implements IHttpPostClient {
   url?: string
+  body?: object
 
   async post (httpPostParams: HttpPostParams): Promise<void> {
     this.url = httpPostParams.url
+    this.body = httpPostParams.body
     return await Promise.resolve()
   }
 }
